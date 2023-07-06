92cc5146ef5f2bc5cef80918754a6e758702b4eb
Kamrul Hasan <63355519+khriad991@users.noreply.github.com>
2023-07-06 23:55:55 +0600

    My-E-Shop Dashboard Project stackture add

---
+++ dashbord/src/App.jsx
@@ -0,0 +1,12 @@
+
+const App = ()=> {
+  return (
+    <div className="flex justify-center justify-items-center h-screen ">
+      <h1 className="text-6xl font-bold  text-indigo-500">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
